b30702c27e372f12baf0048b8633968542da0bf5
mariotristan <mariotristan@gmail.com>
2025-11-28 02:44:24 -0600

    mockit

--- hooks/__tests__/use-user-location.test.ts
+++ hooks/__tests__/use-user-location.test.ts
@@ -1,3 +1,15 @@
+// Mock navigator.geolocation for consistent test results
+beforeAll(() => {
+  const geolocationMock = {
+    getCurrentPosition: jest.fn((success) => {
+      success({
+        coords: { latitude: 25.6866, longitude: -100.3161 },
+      });
+    }),
+  };
+  // @ts-ignore
+  global.navigator.geolocation = geolocationMock;
+});
 import { renderHook } from '@testing-library/react';
 import { useUserLocation } from '../use-user-location';
 
